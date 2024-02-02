3a0ef82128f6abedaaae5da5103f0ff8535f1124
A S B <azzeus917@gmail.com>
2024-02-02 06:41:25 +0530

    first Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import LoginPage from './Pages/LoginScreen'
+import LoginSignupScreen from './Pages/LoginSignupScreen'
+import { Route,Routes } from 'react-router-dom'
+import SignupPage from './Pages/SignUpScreen'
+import ForgotPasswordPage from './Pages/ForgotPassword'
+import HomePage from './Pages/HomePage'
+import ProductOverviewTwo from './Components/ProductOverview'
+import { CartTwo } from './Components/Cart'
+import { CheckoutTwo } from './Components/Checkout'
+import PaymentSuccess from './Pages/PaymentSuccess'
+import PaymentFailure from './Pages/PaymentFailure'
+import { Checkout } from './Components/Checkout2'
+import ProfilePage from './Pages/ProfilePage'
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginSignupScreen />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgotpassword" element={<ForgotPasswordPage/>} />
+        <Route path="/homepage" element={<HomePage/>}/>
+        <Route path="/product/:id" element={<ProductOverviewTwo/>} />
+        <Route path="/cart" element={<CartTwo/>} />
+        <Route path="/checkout" element={<CheckoutTwo/>} />
+        <Route path ='/icheckout'element={<Checkout/>}/>
+        <Route path="/success" element={<PaymentSuccess/>} />
+        <Route path="/failure" element={<PaymentFailure/>} />
+        <Route path="/profilepage" element={<ProfilePage/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
